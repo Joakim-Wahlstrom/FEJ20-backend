7935b45d22f53823527dfea475ba3fafbb4946d7
Isedor021 <joakim.wahlstrom@lexicon.se>
2020-11-09 12:02:14 +0100

    lektion-3

---
+++ lektion-3/controllers/productController.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const productModel = require('../models/products/productModel');
+
+router.get('/', productModel.getProducts)
+router.get('/:id', productModel.getOneProduct)
+
+
+module.exports = router;
---
+++ lektion-3/server.js
@@ -0,0 +1,16 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+
+const port = 9999;
+
+const serverURI = 'http://localhost:' + port;
+const mongoURI = 'mongodb+srv://Joakim:BytMig123@cluster0.aglmi.mongodb.net/REST?retryWrites=true&w=majority';
+
+app.listen(port, () => console.log('Webserver:' + serverURI));
+
+mongoose
+.set('useCreateIndex', true)
+.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => console.log('Connected to DB'))
